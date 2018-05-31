1e2d3a2071b3846e05681d0174f60484c3d04488
MaiReo <sawako.saki@gmail.com>
2018-05-31 17:48:58 +0800

    OMGGGG!

--- www/androidnotificationchannel.js
+++ www/androidnotificationchannel.js
@@ -3,7 +3,7 @@ var exec = require('cordova/exec');
 var AndroidNotificationChannel = {
 	createNotificationChannel: function (channelId, channelName, channelDescription, success, error) {
 			cordova.exec(success, error, 'AndroidNotificationChannel', 'createNotificationChannel', [channelId, channelName, channelDescription]);
-	}
+	},
 	AndroidNotificationChannel: undefined
 };
 AndroidNotificationChannel.AndroidNotificationChannel = AndroidNotificationChannel;
