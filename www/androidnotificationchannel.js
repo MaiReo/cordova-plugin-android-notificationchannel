da70714ae1e0fac747af52c1c432efbac5aff1d8
MaiReo <sawako.saki@gmail.com>
2018-05-31 17:36:59 +0800

    OMG!

--- www/androidnotificationchannel.js
+++ www/androidnotificationchannel.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
 
-exports.exports = {
+module.exports = {
 	AndroidNotificationChannel: {
 		createNotificationChannel: function (channelId, channelName, channelDescription, success, error) {
 			cordova.exec(success, error, 'AndroidNotificationChannel', 'createNotificationChannel', [channelId, channelName, channelDescription]);
